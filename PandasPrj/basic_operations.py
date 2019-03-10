f22cd7b0520d1d1403bd9bc5d2810aea402b48ad
Charanjeet Arneja <charanjeetsingharneja@gmail.com>
2019-03-10 14:45:25 +0530

    Updated for capcaclc engine

--- PandasPrj/basic_operations.py
+++ PandasPrj/basic_operations.py
@@ -11,6 +11,8 @@ datadf=pd.read_csv('C:\Data\PythonCode\Data\data.csv')
 
 rulesdf=pd.read_csv("C:\Data\PythonCode\Data\\rules.csv")
 
+rodf=pd.read_csv("C:\Data\PythonCode\Data\\rules_output.csv")
+
 rulesdf.sort_values('RULE_RK',ascending=True, inplace=True)
 
 output=datadf.query('emp==-1')
@@ -23,7 +25,37 @@ for r in rulesdf.values:
         query=str(r[1])+' != ' + str(r[3])
     dd=datadf.query(query)
     dd["RULE_RK"]=str(r[0])
+    dd["RULE_PRIORITY"] = str(r[4])
     output=output.append(dd)
     #print(dd)
 
 header(output)
+
+high_priority_rule=output.groupby(['emp'],as_index=False).max()
+header(high_priority_rule)
+
+header(rodf)
+
+#print(rodf.lookup(rodf.index, rodf['RULE_RK']))
+
+def func(df,k,f):
+    #df[k]
+    return(0)
+
+for t in high_priority_rule.values:
+    rule=t[1]
+    key=t[0]
+    query='RULE_RK =='+str(rule)
+    n=rodf.query(query)
+    #print(t["ATTR"].values[0])
+    if n["ATTR"].values[0] not in high_priority_rule:
+        high_priority_rule.insert(0, n["ATTR"].values[0], func(high_priority_rule,key,n["VALUE"].values[0]))
+    print(high_priority_rule.where(query))
+
+    #[n["ATTR"].values[0]])
+    #print(high_priority_rule)
+    #output[t["ATTR"].values]=0
+    #print(t["ATTR"].values)
+
+
+#print(high_priority_rule)
